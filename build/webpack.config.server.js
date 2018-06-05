d98b9132224989794b2b606a7846cd3898d54c2e
langyanping <langyanping@rtmap.com>
2018-06-05 13:58:30 +0800

    配置node端的webpack文件

---
+++ build/webpack.config.server.js
@@ -0,0 +1,75 @@
+const path = require('path')
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.config.base')
+const ExtractPlugin = require('extract-text-webpack-plugin')
+const VueServerPlugin = require('vue-server-renderer/server-plugin')
+
+let config
+
+config = merge(baseConfig, {
+  target: 'node',
+  entry: path.join(__dirname, '../client/server-entry.js'),
+  devtool: 'source-map',
+  output: {
+    libraryTarget: 'commonjs2', // export.module
+    filename: 'server-entry.js', // 将node_modules里的依赖都打包到server-entery.js文件中
+    path: path.join(__dirname, '../server-build')
+  },
+  // 因为在浏览器环境中不能使用require引入代码，但是现在在node端是可以直接用require引用node_module文件，所以在弄的环境去掉依赖项。
+  externals: Object.keys(require('../package.json').dependencies),
+  module: {
+    // rules: [
+    //   {
+    //     // 将css单独打包，这里不能使用style-loader，以为style-loader是将css用js引入dom插到HTML中的,
+    //     // 而在node环境，是没有dom的执行环境，就会报错，所以就用vue-style-loader。
+    //     test: /\.styl/,
+    //     use: [
+    //       'vue-style-loader',
+    //       'css-loader',
+    //       {
+    //         loader: 'postcss-loader',
+    //         options: {
+    //           sourceMap: true
+    //         }
+    //       },
+    //       'stylus-loader'
+    //     ]
+    //   }
+    // ]
+    rules: [{
+      test: /\.styl/,
+      use: ExtractPlugin.extract({
+        fallback: 'vue-style-loader',
+        use: [
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          'stylus-loader'
+        ]
+      })
+    }]
+  },
+  // import Vue from 'vue'
+  // resolve: { 不需要指定alias
+  //   alias: {
+  //     'vue': path.join(__dirname, '../node_modules/vue/dist/vue.esm.js')
+  //   }
+  // },
+  plugins: [
+    // new webpack.HotModuleReplacementPlugin(), 不需要
+    // new webpack.NoEmitOnErrorsPlugin(), 不需要
+    new ExtractPlugin('styles.[chunkhash:8].css'),
+    new webpack.DefinePlugin({ // 有可能在文件中会用到这两个变量
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+      'process.env.VUE_ENV': '"server"'
+    }),
+    new VueServerPlugin()
+  ]
+})
+
+module.exports = config
