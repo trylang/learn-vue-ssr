e7b36ecd5905a73f6cd8dba462bd0b022acc9eca
langyanping <langyanping@rtmap.com>
2018-06-19 18:59:06 +0800

    学习服务端api请求

--- server/db/db.js
+++ server/db/db.js
@@ -3,6 +3,19 @@ const axios = require('axios')
 
 const className = 'todo' // 线上数据库的命名空间，知道操作哪个数据库
 
-const request = axios.create({
-  baseURL: ''
+const request = axios.create({ // 参照文档https://docs.apicloud.com/Cloud-API/data-cloud-api
+  baseURL: 'https://d.apicloud.com/mcm/api/'
 })
+
+module.exports = (appId, appKey) => {
+  const getHeaders = () => {
+    const now = Date.now()
+    return {
+      'X-APICloud-AppId': appId,
+      'X-APICloud-AppKey': `${sha1(`${appId}UZ${appKey}UZ${now}`)}.${now}`
+    }
+  }
+  return {
+
+  }
+}
